d1d0fd56637783e08f9ad5ddffe8c2d83b0c4088
Lucas <lucasvinicios-fs@hotmail.com>
2020-08-21 21:05:48 -0300

    Class Model

--- src/main/java/com/algaworks/osworks/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/model/Cliente.java
@@ -1,10 +1,31 @@
 package com.algaworks.osworks.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Size(max = 60)
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
     public Long getId() {
@@ -37,5 +58,18 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cliente)) return false;
+        Cliente cliente = (Cliente) o;
+        return getId().equals(cliente.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
